7348a452d536018482e76cb0d3770addefc9cf33
Mario Alberto Negrete Rodríguez <mario_rguezz@yahoo.com.mx>
2017-02-13 16:59:53 -0600

    first commit

---
+++ Byond/Assets/DataLoader.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataLoader : MonoBehaviour {
+
+	public string inputUserName;
+	public string inputPassword;
+
+	string LoginURL = "localhost/SeminariosGit2/api_php/public/usuario/authenticate";
+
+
+	IEnumerator Start(){
+		//LoginToDB ();
+		WWWForm form = new WWWForm ();
+		form.AddField ("email", "xx");
+		//form.AddField ("password", inputPassword);
+
+		WWW www = new WWW(LoginURL,form);
+		yield return www;
+		Debug.Log(www.text);
+}
+
+	void Update(){
+		
+	}
+
+
+	/*IEnumerator LoginToDB(){
+		WWWForm form = new WWWForm ();
+		form.AddField ("email", "xx");
+		//form.AddField ("password", inputPassword);
+
+		WWW www = new WWW(LoginURL,form);
+		yield return www;
+		Debug.Log(www.text);
+	}*/
+
+}
